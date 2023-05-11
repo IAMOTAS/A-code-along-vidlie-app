f5429c234574a0f733996d28cf2b675213caca89
IAMOTAS <niiotasalokome@gmail.com>
2023-05-11 14:44:13 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace A_code_along_vidlie_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
