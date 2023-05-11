c5667622bc68295ebe1b90f1ed704c88a98012a3
IAMOTAS <niiotasalokome@gmail.com>
2023-05-11 17:50:15 +0000

    For starters

---
+++ Controllers/MoviesController1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using A_code_along_vidlie_app.Models;
+
+namespace A_code_along_vidlie_app.Controllers
+{
+    public class MoviesController1 : Controller
+    {
+        //GET:Movies/Random
+        public IActionResult Random()
+        {
+            var movie = new Movies()
+            {
+                Name = "Shrek"
+            };
+            return View();
+        }
+    }
+}
---
+++ Models/Movies.cs
@@ -0,0 +1,9 @@
+﻿namespace A_code_along_vidlie_app.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    //movies-random
+}
